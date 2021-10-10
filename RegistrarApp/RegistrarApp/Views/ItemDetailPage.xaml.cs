bc1c6cef93bcdde4849c0cc177b1eeca2d6166c5
Christopher Likins <christopher.likins@gmail.com>
2021-10-10 14:13:31 -0700

    Add project files.

---
+++ RegistrarApp/RegistrarApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RegistrarApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace RegistrarApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ RegistrarApp/RegistrarApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using RegistrarApp.Models;
+using RegistrarApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RegistrarApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
